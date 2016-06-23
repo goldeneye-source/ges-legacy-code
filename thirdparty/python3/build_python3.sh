8f14ecb1c098c39c7f82f5501348543956c3fe51
Jonathan White <support@dmapps.us>
2016-06-22 21:55:21 -0400

    Finalized cmake scripts

--- thirdparty/python3/build_python3.sh
+++ thirdparty/python3/build_python3.sh
@@ -14,14 +14,9 @@ else
   sleep 1
 fi
 
-if ! [ -f ./bin/lib/libpython3.4m.so.1.0 ]; then
-  # Remove leftovers if they exist
-  if [ -f "../Include/pyconfig.h" ]; then
-    rm ../Include/pyconfig.h
-  fi
-
+if ! [ -f ./libpython3.4m.a ]; then
   # Build Python
-  make
+  make -j 4
   make install
 else
   echo "No need to build Python!"
@@ -36,8 +31,7 @@ set -x
 cp -v pyconfig.h ../../../public/python/pyconfig.h
 
 # Copy the shared library (eventually deploys to $GES_PATH/bin)
-chmod 0664 ./bin/lib/libpython*
-cp -v ./bin/lib/libpython3.so ../../../lib/python/
+cp -v libpython3.4m.a ../../../lib/python/
 
 set +x 
 
